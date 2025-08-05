3950875e25afdf333516a3a37c2a52adb555d4df
omrakholiya77 <omrakholiya950@gmail.com>
2025-08-05 11:23:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (27.34%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,10 +1,31 @@
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[i].length;j++){
+//                 if(matrix[i][j]==target){
+//                     return true;
+//                 }
+//             }
+//         }return false;
+//     }
+// }
+
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int i=0;
+        int j=col-1;
+        while(i<row && j>=0){
+            if(matrix[i][j]==target){
+                return true;
+            }
+            else if(matrix[i][j]<target){
+                i++;
+            }
+            else{
+                j--;
             }
         }return false;
     }
