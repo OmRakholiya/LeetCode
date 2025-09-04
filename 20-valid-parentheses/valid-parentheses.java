757dd5ba86bd5cd0895d68d894f97abb329da7ff
omrakholiya77 <omrakholiya950@gmail.com>
2025-09-04 18:47:04 +0530

    Time: 1 ms (99.49%) | Memory: 41.3 MB (99.57%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,71 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
+//             s = s.replace("()", "")
+//                  .replace("{}", "")
+//                  .replace("[]", "");
+//         }
+//         return s.isEmpty();
+//     }
+// }
+
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack = new Stack<>();
+//         for(char c:s.toCharArray()){
+//             if(c=='(' || c=='{' || c=='['){
+//                 stack.push(c);
+//             }
+//             else{
+//                 if(stack.isEmpty()){
+//                     return false;
+//                 }
+//                 char top = stack.pop();
+//                 if((c==')' && top!='(') || (c=='}'&& top!='{') || (c==']' && top!='[')){
+//                     return false;
+//                 }
+//             }
+//         }return stack.isEmpty();
+//     }
+// }
+
+// class Solution {
+//     public boolean isValid(String s) {
+//        Map<Character, Character> map = new HashMap<>();
+//         map.put(')', '(');
+//         map.put('}', '{');
+//         map.put(']', '[');
+
+//         Stack<Character> stack = new Stack<>();
+
+//         for (char c : s.toCharArray()) {
+//             if (map.containsKey(c)) {
+//                 char top = stack.isEmpty() ? '#' : stack.pop();
+//                 if (top != map.get(c)) return false;
+//             } else {
+//                 stack.push(c);
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+
+
+class Solution {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+        for(char c:s.toCharArray()){
+            if(c=='('  || c=='{'  || c=='['){
+                stack[++top] = c;
+            }
+            else{
+                if(top==-1)  return false;
+                char open = stack[top--];
+                 if((c==')' && open!='(') || (c=='}'&& open!='{') || (c==']' && open!='[')){
+                    return false;
+                }
+            }
+        }return top==-1;
+    }
+}
