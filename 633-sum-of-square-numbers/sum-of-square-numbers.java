0108e33bce161382531bf92afaac16b19e373ace
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-09-20 09:44:25 +0530

    Time: 3 ms (94.91%) | Memory: 40.5 MB (62.10%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,11 +1,26 @@
+// class Solution {
+//     public boolean judgeSquareSum(int c) {
+//         for(long i=0;i*i<=c;i++){
+//             long j=c-i*i;
+//             long sqrtj = (long)Math.sqrt(j);
+//             if(sqrtj*sqrtj == j){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean judgeSquareSum(int c) {
-        for(long i=0;i*i<=c;i++){
-            long j=c-i*i;
-            long sqrtj = (long)Math.sqrt(j);
-            if(sqrtj*sqrtj == j){
-                return true;
-            }
+        long l=0;
+        long r=(long)Math.sqrt(c);
+        while(l<=r){
+            long sum = l*l + r*r;
+            if(sum==c)  return true;
+            else if(sum<c)  l++;
+            else r--;
         }
         return false;
     }
