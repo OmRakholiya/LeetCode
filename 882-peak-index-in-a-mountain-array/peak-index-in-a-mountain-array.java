d18891d879e34668b7acbd6be5966cb87ea58696
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-09-22 17:37:14 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (82.26%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,20 +1,39 @@
+// class Solution {
+//     public int peakIndexInMountainArray(int[] arr) {
+//         int l=1;
+//         int r=arr.length-2;
+//         while(l<=r){
+//             int mid=l+(r-l)/2;
+//             if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
+//                 return mid;
+//             }
+//             else if(arr[mid]>arr[mid-1]){
+//                 l=mid+1;
+//             }
+//             else{
+//                 r=mid-1;
+//             }
+//         }
+//         return 0;
+//     }
+// }
+
+
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int l=1;
-        int r=arr.length-2;
-        while(l<=r){
+        int l=0;
+        int r=arr.length-1;
+        while(l<r){
             int mid=l+(r-l)/2;
-            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
-                return mid;
-            }
-            else if(arr[mid]>arr[mid-1]){
+
+            if(arr[mid] < arr[mid+1]){
                 l=mid+1;
             }
             else{
-                r=mid-1;
+                r=mid;
             }
         }
-        return 0;
+        return l;
     }
 }
 
