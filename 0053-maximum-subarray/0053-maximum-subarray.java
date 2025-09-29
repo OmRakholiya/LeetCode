2d658cd8d0450463bfb013e4676bc846d5827940
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-09-29 20:02:04 +0530

    Time: 1 ms (99.44%), Space: 57.6 MB (8.29%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curr=nums[0];
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            curr=Math.max(nums[i],curr+nums[i]);
+            max=Math.max(max,curr);
+        }
+        return max;
+    }
+}
+
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int maxsum=Integer.MIN_VALUE;
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i;j<nums.length;j++){
+//                 int sum=0;
+//                 for(int k=i;k<=j;k++){                    
+//                     sum+=nums[k];
+//                 }
+//                 maxsum=Math.max(maxsum,sum);
+//             }
+//         }return maxsum;
+//     }
+// }
