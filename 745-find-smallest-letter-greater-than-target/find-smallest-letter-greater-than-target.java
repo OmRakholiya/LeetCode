ddd13df06f2dcbbb1e819ccb7d161f7a27920bd7
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-10-01 13:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (27.64%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -12,18 +12,29 @@
 
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        int l=0;
-        int r = letters.length-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(letters[mid]<=target){
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
+        for(char ch : letters){
+            if(ch>target){
+                return ch;
             }
         }
-        return letters[l%letters.length];
+        return letters[0];
     }
 }
+
+// class Solution {
+//     public char nextGreatestLetter(char[] letters, char target) {
+//         int l=0;
+//         int r = letters.length-1;
+//         while(l<=r){
+//             int mid=l+(r-l)/2;
+//             if(letters[mid]<=target){
+//                 l=mid+1;
+//             }
+//             else{
+//                 r=mid-1;
+//             }
+//         }
+//         return letters[l%letters.length];
+//     }
+// }
 
