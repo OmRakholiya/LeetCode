0bfa2958d483ba40bd1730876b824c3a2280d529
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-10-01 14:20:36 +0530

    Time: 2 ms (93.07%) | Memory: 42.9 MB (59.42%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,12 +1,27 @@
+// class Solution {
+//     public boolean checkIfExist(int[] arr) {
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr.length;j++){
+//                 if(i!=j && arr[i] == 2 * arr[j]){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+import java.util.Arrays;
 class Solution {
     public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(i!=j && arr[i] == 2 * arr[j]){
-                    return true;
-                }
+            int target = 2*arr[i];
+            int index=Arrays.binarySearch(arr,target);
+            if(index>=0 && index!=i){
+                return true;
             }
         }
         return false;
     }
+
 }
