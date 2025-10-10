56df7b8ee9d98e5b16c8f9337017317561937108
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-10-10 09:26:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (10.56%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,16 +1,42 @@
+// class Solution {
+//     public int specialArray(int[] nums) {
+//         for(int x=0;x<=nums.length;x++){
+//             int count=0;
+//             for(int num : nums){
+//                 if(num>=x){
+//                     count++;
+//                 }
+//             }
+//             if(count==x){
+//                 return x;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int specialArray(int[] nums) {
-        for(int x=0;x<=nums.length;x++){
-            int count=0;
-            for(int num : nums){
-                if(num>=x){
-                    count++;
+            int l=0;
+            int r=nums.length;
+            while(l<=r){
+                int mid=l+(r-l)/2;
+                int count=0;
+                for(int num:nums){
+                    if(num>=mid){
+                        count++;
+                    }
+                }
+                if(count==mid){
+                    return mid;
+                }
+                else if(count>mid){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
                 }
             }
-            if(count==x){
-                return x;
-            }
-        }
-        return -1;
+            return -1;
     }
 }
