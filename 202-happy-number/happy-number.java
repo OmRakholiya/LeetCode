e23283a6596f6fe29977b41917ea281be5718662
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-11-07 10:21:46 +0530

    Time: 1 ms (66.14%) | Memory: 42.1 MB (7.10%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,31 +1,14 @@
-// class Solution {
-//     public boolean isHappy(int n) {
-//        int slow=n;
-//        int fast=n;
-//        while(slow!=fast){
-//         slow=squre(slow);
-//         fast=squre(squre(fast));
-//        }
-//        return slow==1;
-//     }
-//     public int squre(int num){
-//         int ans=0;
-//         while(num>0){
-//             int r=num%10;
-//             ans+=r*r;
-//             num/=10;
-//         }return ans;
-//     }
-// }
-
 class Solution {
     public boolean isHappy(int n) {
-        HashSet<Integer> seen = new HashSet<>();
-        while(n!=1 && !seen.contains(n)){
-            seen.add(n);
-            n=squre(n); 
-        }
-        return n==1;
+        int slow=n;
+        int fast=n;
+        do{
+            slow=squre(slow);
+            fast=squre(squre(fast));
+            if(fast==1)  return true;
+        }while(slow!=fast);
+
+        return slow==1;
     }
     public int squre(int num){
         int ans=0;
@@ -36,3 +19,22 @@ class Solution {
         }return ans;
     }
 }
+
+// class Solution {
+//     public boolean isHappy(int n) {
+//         HashSet<Integer> seen = new HashSet<>();
+//         while(n!=1 && !seen.contains(n)){
+//             seen.add(n);
+//             n=squre(n); 
+//         }
+//         return n==1;
+//     }
+//     public int squre(int num){
+//         int ans=0;
+//         while(num>0){
+//             int digit=num%10;
+//             ans+=digit*digit;
+//             num/=10;
+//         }return ans;
+//     }
+// }
