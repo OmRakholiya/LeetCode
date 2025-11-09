7fd53ddd02a2c9b8887169c2007d96c02eef1638
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-11-09 16:23:01 +0530

    Time: 6 ms (54.66%) | Memory: 43.7 MB (6.39%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,21 +1,39 @@
+// class Solution {
+//     public int longestPalindrome(String s) {
+//         Map<Character,Integer> map = new HashMap<>();
+//         for(char c:s.toCharArray()){
+//             map.put(c,map.getOrDefault(c,0)+1);
+//         }
+//         int len=0;
+//         boolean found=false;
+//         for(int count : map.values()){
+//             if(count%2==0){
+//                 len+=count;
+//             }
+//             else{
+//                 len+=count-1;
+//                 found=true;
+//             }
+//         }
+//         if(found) len+=1;
+//         return len;
+//     }
+// }
+
 class Solution {
     public int longestPalindrome(String s) {
-        Map<Character,Integer> map = new HashMap<>();
-        for(char c:s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
+        Set<Character> set = new HashSet<>();
         int len=0;
-        boolean found=false;
-        for(int count : map.values()){
-            if(count%2==0){
-                len+=count;
+        for(char c : s.toCharArray()){
+            if(set.contains(c)){
+                set.remove(c);
+                len+=2;
             }
             else{
-                len+=count-1;
-                found=true;
+                set.add(c);
             }
         }
-        if(found) len+=1;
+        if(!set.isEmpty())  len+=1;
         return len;
     }
 }
