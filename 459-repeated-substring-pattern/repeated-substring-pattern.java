39e1e8fcbe19c6863840a136cf9e098859ff4741
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-11-10 20:34:46 +0530

    Time: 30 ms (75.40%) | Memory: 46.9 MB (6.65%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,21 +1,21 @@
-// class Solution {
-//     public boolean repeatedSubstringPattern(String s) {
-//         int n=s.length();
-//         for(int i=1;i<=n/2;i++){
-//             if(n % i == 0){
-//                 String sub = s.substring(0,i);
-//                 StringBuilder sb = new StringBuilder();
-//                 int repetCount=n/i;
-//                 for(int j=0;j<repetCount;j++){
-//                     sb.append(sub);
-//                 }
-//                 if(sb.toString().equals(s)){
-//                     return true;
-//                 }
-//             }
-//         }return false;
-//     }
-// }
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n=s.length();
+        for(int i=1;i<=n/2;i++){
+            if(n % i == 0){
+                String sub = s.substring(0,i);
+                StringBuilder sb = new StringBuilder();
+                int repetCount=n/i;
+                for(int j=0;j<repetCount;j++){
+                    sb.append(sub);
+                }
+                if(sb.toString().equals(s)){
+                    return true;
+                }
+            }
+        }return false;
+    }
+}
 
 // class Solution {
 //     public boolean repeatedSubstringPattern(String s) {
@@ -24,8 +24,8 @@
 //     }
 // }
 
-class Solution {
-    public boolean repeatedSubstringPattern(String s) {
-        return s.matches("(.+)\\1+");
-    }
-}
+// class Solution {
+//     public boolean repeatedSubstringPattern(String s) {
+//         return s.matches("(.+)\\1+");
+//     }
+// }
