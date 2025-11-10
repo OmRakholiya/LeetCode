276d7f69a0045f0a86561fb35d2af84fe9192908
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-11-10 20:33:05 +0530

    Time: 75 ms (45.26%) | Memory: 46.8 MB (6.65%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -0,0 +1,25 @@
+// class Solution {
+//     public boolean repeatedSubstringPattern(String s) {
+//         int n=s.length();
+//         for(int i=1;i<=n/2;i++){
+//             if(n % i == 0){
+//                 String sub = s.substring(0,i);
+//                 StringBuilder sb = new StringBuilder();
+//                 int repetCount=n/i;
+//                 for(int j=0;j<repetCount;j++){
+//                     sb.append(sub);
+//                 }
+//                 if(sb.toString().equals(s)){
+//                     return true;
+//                 }
+//             }
+//         }return false;
+//     }
+// }
+
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        return doubled.substring(1,doubled.length()-1).contains(s);
+    }
+}
