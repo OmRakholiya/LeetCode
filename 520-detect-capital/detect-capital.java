4d738c36328d1c0000a945a188819749688cad5b
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-11-10 21:56:08 +0530

    Time: 5 ms (4.65%) | Memory: 43.2 MB (5.96%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -1,15 +1,25 @@
+// class Solution {
+//     public boolean detectCapitalUse(String word) {
+//         int upperCount =0;
+//         for(char c: word.toCharArray()){
+//             if(Character.isUpperCase(c)){
+//                 upperCount++;
+//             }
+//         }
+//         int n=word.length();
+//         if(upperCount==n) return true;
+//         if(upperCount==0) return true;
+//         if(upperCount==1 && Character.isUpperCase(word.charAt(0))) return true;
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean detectCapitalUse(String word) {
-        int upperCount =0;
-        for(char c: word.toCharArray()){
-            if(Character.isUpperCase(c)){
-                upperCount++;
-            }
-        }
-        int n=word.length();
-        if(upperCount==n) return true;
-        if(upperCount==0) return true;
-        if(upperCount==1 && Character.isUpperCase(word.charAt(0))) return true;
-        return false;
+         return word.equals(word.toUpperCase()) ||
+               word.equals(word.toLowerCase()) ||
+               word.equals(Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase());
     }
 }
+
