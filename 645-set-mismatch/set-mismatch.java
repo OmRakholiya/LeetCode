5bc7b0975613290aaafeb609d8deb6da60b238c4
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-11-14 21:49:38 +0530

    Time: 14 ms (20.55%) | Memory: 47.2 MB (15.24%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,15 +1,35 @@
+// class Solution {
+//     public int[] findErrorNums(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         int dup=-1,missing=-1;
+//         for(int num : nums){
+//             map.put(num,map.getOrDefault(num,0)+1);
+//         }
+//         for(int i=1;i<=nums.length;i++){
+//             int freq=map.getOrDefault(i,0);
+//             if(freq==2) dup = i;
+//             if(freq==0) missing = i;
+//         }
+//         return new int[]{dup,missing};
+//     }
+// }   
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int dup=-1,missing=-1;
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+        HashSet<Integer> set = new HashSet<>();
+        int dup = -1;
+        int sum = 0;
+
+        for (int num : nums) {
+            if (set.contains(num)) dup = num;
+            else set.add(num);
+            sum += num;
         }
-        for(int i=1;i<=nums.length;i++){
-            int freq=map.getOrDefault(i,0);
-            if(freq==2) dup = i;
-            if(freq==0) missing = i;
-        }
-        return new int[]{dup,missing};
+
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+
+        int missing = expectedSum - (sum - dup);
+        return new int[]{dup, missing};
     }
 }
