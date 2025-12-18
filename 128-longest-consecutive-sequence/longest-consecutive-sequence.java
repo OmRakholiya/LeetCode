7bfbc49a7962a829e9b3012390197f588053fe65
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-18 15:26:37 +0530

    Time: 38 ms (19.96%) | Memory: 96.2 MB (7.23%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,20 +1,70 @@
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+
+//         if(nums.length == 0) return 0;
+//         Arrays.sort(nums);
+//         int longest = 1;
+//         int count = 1;
+
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[i]==nums[i-1]){
+//                 continue;
+//             }
+//             else if(nums[i]==nums[i-1]+1){
+//                 count++;
+//             }
+//             else{
+//                 count=1;
+//             }
+//             longest=Math.max(longest,count);
+//         }
+//         return longest;
+//     }
+// }
+
+
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         if(nums.length==0) return 0;
+//         Arrays.sort(nums);
+//         int lastsmaller=Integer.MIN_VALUE;
+//         int longest=1;
+//         int count=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]-1==lastsmaller){
+//                 count++;
+//                 lastsmaller=nums[i];
+//             }
+//             else if(lastsmaller!=nums[i]){
+//                 count=1;
+//                 lastsmaller=nums[i];
+//             }
+//             longest=Math.max(longest,count);
+//         }
+//         return longest;
+//     }
+// }
+
+
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length==0) return 0;
-        Arrays.sort(nums);
-        int lastsmaller=Integer.MIN_VALUE;
-        int longest=1;
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]-1==lastsmaller){
-                count++;
-                lastsmaller=nums[i];
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        int longest=0;
+        for(int num : set){
+            if(!set.contains(num-1)){
+                int current = num;
+                int count = 1;
+                while(set.contains(current+1)){
+                    current++;
+                    count++;
+                }
+                longest = Math.max(longest,count);
             }
-            else if(lastsmaller!=nums[i]){
-                count=1;
-                lastsmaller=nums[i];
-            }
-            longest=Math.max(longest,count);
         }
         return longest;
     }
