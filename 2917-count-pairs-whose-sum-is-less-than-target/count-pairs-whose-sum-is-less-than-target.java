ce14c5f9195fb8998e84be027775dd8939477e3a
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-19 18:27:28 +0530

    Time: 2 ms (95.14%) | Memory: 44.2 MB (31.78%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
@@ -1,37 +1,37 @@
-// class Solution {
-//     public int countPairs(List<Integer> nums, int target) {
-//         int count=0;
-//         for(int i=0;i<nums.size();i++){
-//             for(int j=i+1;j<nums.size();j++){
-//                 if(nums.get(i)+nums.get(j)<target){
-//                     count++;
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// }
-
-
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
-        int[] arr = new int[nums.size()];
-        for (int i = 0; i < nums.size(); i++) {
-            arr[i] = nums.get(i);
-        }
-        Arrays.sort(arr);
-        int i=0;
-        int j=arr.length-1;
         int count=0;
-        while(i<j){
-            if(arr[i] + arr[j] < target){
-                count+=(j-i);
-                i++;
-            }
-            else{
-                j--;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums.get(i)+nums.get(j)<target){
+                    count++;
+                }
             }
         }
         return count;
     }
 }
+
+
+// class Solution {
+//     public int countPairs(List<Integer> nums, int target) {
+//         int[] arr = new int[nums.size()];
+//         for (int i = 0; i < nums.size(); i++) {
+//             arr[i] = nums.get(i);
+//         }
+//         Arrays.sort(arr);
+//         int i=0;
+//         int j=arr.length-1;
+//         int count=0;
+//         while(i<j){
+//             if(arr[i] + arr[j] < target){
+//                 count+=(j-i);
+//                 i++;
+//             }
+//             else{
+//                 j--;
+//             }
+//         }
+//         return count;
+//     }
+// }
