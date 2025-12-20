c356125d27d00bf2641d4c9eb32012c465f8ccd7
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-20 16:14:53 +0530

    Time: 1 ms (61.98%) | Memory: 43.2 MB (38.08%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> set = new HashSet<>();
+        for(int i=0;i<jewels.length();i++){
+            set.add(jewels.charAt(i));
+        }
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            if(set.contains(stones.charAt(i))){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+// class Solution {
+//     public int numJewelsInStones(String jewels, String stones) {
+//         int count=0;
+//         for(int i=0;i<jewels.length();i++){
+//             for(int j=0;i<stones.length();j++){
+//                 if(jewels.charAt(i)==stones.charAt(j)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
