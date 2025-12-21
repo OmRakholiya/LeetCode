7eb9a08dc78617094fc6270d2e11c51a5a83c31f
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-21 15:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (47.73%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,25 +1,52 @@
+// class Solution {
+//     public int[] relativeSortArray(int[] arr1, int[] arr2) {
+//         ArrayList<Integer> l = new ArrayList<>();
+//         for(int x : arr2){
+//             for(int i=0;i<arr1.length;i++){
+//                 if(arr1[i]==x){
+//                     l.add(arr1[i]);
+//                     arr1[i]=-1;
+//                 }
+//             }
+//         }
+//         Arrays.sort(arr1);
+//         for(int x:arr1){
+//             if(x!=-1){
+//                 l.add(x);
+//             }
+//         }
+
+//         int[] result = new int[l.size()];
+//         for(int i=0;i<l.size();i++){
+//             result[i]=l.get(i);
+//         }
+//         return result;
+//     }
+// }
+
+
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        ArrayList<Integer> l = new ArrayList<>();
-        for(int x : arr2){
-            for(int i=0;i<arr1.length;i++){
-                if(arr1[i]==x){
-                    l.add(arr1[i]);
-                    arr1[i]=-1;
-                }
-            }
-        }
-        Arrays.sort(arr1);
+        int[] count = new int[1001];
         for(int x:arr1){
-            if(x!=-1){
-                l.add(x);
+            count[x]++;
+        }
+        int[] res = new int[arr1.length];
+        int index=0;
+
+        for(int x : arr2){
+            while(count[x]-- > 0){
+                res[index]=x;
+                index++;
             }
         }
 
-        int[] result = new int[l.size()];
-        for(int i=0;i<l.size();i++){
-            result[i]=l.get(i);
+        for(int i=0;i<count.length;i++){
+            while(count[i]-- >0){
+                res[index]=i;
+                index++;
+            }
         }
-        return result;
+        return res;
     }
 }
