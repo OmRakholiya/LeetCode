e2162efe361639bf03c0c69f4418218b558357b2
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-22 18:50:34 +0530

    Time: 8 ms (88.76%) | Memory: 49 MB (42.94%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,3 +1,32 @@
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+//         int n = intervals.length;
+//         Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
+
+//         List<int[]> res = new ArrayList<>();
+
+//         for(int i=0;i<n;i++){
+//             int start = intervals[i][0];
+//             int end = intervals[i][1];
+
+//             if(!res.isEmpty() && end<=res.get(res.size()-1)[1]){
+//                 continue;
+//             }
+//             for(int j=i+1;j<n;j++){
+//                 if(intervals[j][0]<=end){
+//                     end=Math.max(end,intervals[j][1]);
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//             res.add(new int[]{start,end});
+//         } 
+//         return res.toArray(new int[res.size()][]);
+//     }
+// }
+
+
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
@@ -6,21 +35,12 @@ class Solution {
         List<int[]> res = new ArrayList<>();
 
         for(int i=0;i<n;i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if(!res.isEmpty() && end<=res.get(res.size()-1)[1]){
-                continue;
+            if(res.isEmpty() || intervals[i][0] > res.get(res.size()-1)[1]){
+                res.add(intervals[i]);
             }
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=end){
-                    end=Math.max(end,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                res.get(res.size()-1)[1]=Math.max(intervals[i][1],res.get(res.size()-1)[1]);
             }
-            res.add(new int[]{start,end});
         } 
         return res.toArray(new int[res.size()][]);
     }
