8c33a7301035a065cb636b60c49603f90844d1bd
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-23 18:52:27 +0530

    Time: 2 ms (87.97%) | Memory: 47.9 MB (26.57%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,30 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int index=0;
+//         for(int num : nums){
+//             if(num != 0){
+//                 nums[index]=num;
+//                 index++;
+//             }
+//         }
+//         while(index<nums.length){
+//             nums[index]=0;
+//             index++;
+//         }
+//     }
+// }
+
 class Solution {
     public void moveZeroes(int[] nums) {
-        int index=0;
-        for(int num : nums){
-            if(num != 0){
-                nums[index]=num;
-                index++;
+        int lastNonZero=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int tem = nums[lastNonZero];
+                nums[lastNonZero] = nums[i];
+                nums[i] = tem;
+
+                lastNonZero++;
             }
-        }
-        while(index<nums.length){
-            nums[index]=0;
-            index++;
         }
     }
 }
