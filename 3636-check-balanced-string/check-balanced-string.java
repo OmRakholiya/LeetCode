c881f48c42f34b2b2c8d93cbd24690287698ac1a
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-26 18:27:49 +0530

    Time: 1 ms (98.59%) | Memory: 43.6 MB (40.77%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.java
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -1,14 +1,30 @@
+// class Solution {
+//     public boolean isBalanced(String num) {
+//         char[] arr = num.toCharArray();
+//         int sumofeven=0;
+//         int sumofodd=0;
+//         for(int i=0;i<arr.length;i++){
+//             if(i%2==0){
+//                 sumofeven+=arr[i]-'0';
+//             }
+//             if(i%2!=0){
+//                 sumofodd+=arr[i]-'0';
+//             } 
+//         }
+//         return sumofeven==sumofodd;
+//     }
+// }
+
 class Solution {
     public boolean isBalanced(String num) {
-        char[] arr = num.toCharArray();
         int sumofeven=0;
         int sumofodd=0;
-        for(int i=0;i<arr.length;i++){
-            if(i%2==0){
-                sumofeven+=arr[i]-'0';
+        for(int i=0;i<num.length();i++){
+            if((i&1)==0){
+                sumofeven+=num.charAt(i)-'0';
             }
-            if(i%2!=0){
-                sumofodd+=arr[i]-'0';
+            else{
+                sumofodd+=num.charAt(i)-'0';
             } 
         }
         return sumofeven==sumofodd;
