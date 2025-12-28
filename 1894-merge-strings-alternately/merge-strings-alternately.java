ba8652d8a2d8a2406d1fc4298be9e602f420f91c
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-28 11:19:15 +0530

    Time: 1 ms (94.34%) | Memory: 43.1 MB (40.79%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,18 +1,37 @@
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+//         String s = "";
+//         int i=0;
+//         int j=0;
+//         while(i<word1.length() || j<word2.length()){
+//             if(i<word1.length()){
+//                 s+=word1.charAt(i);
+//                 i++;
+//             }
+//             if(j<word2.length()){
+//                 s+=word2.charAt(j);
+//                 j++;
+//             }
+//         }
+//         return s;
+//     }
+// }
+
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String s = "";
+        StringBuilder sb = new StringBuilder();
         int i=0;
         int j=0;
         while(i<word1.length() || j<word2.length()){
             if(i<word1.length()){
-                s+=word1.charAt(i);
+                sb.append(word1.charAt(i));
                 i++;
             }
             if(j<word2.length()){
-                s+=word2.charAt(j);
+                sb.append(word2.charAt(j));
                 j++;
             }
         }
-        return s;
+        return sb.toString();
     }
 }
