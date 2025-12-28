ec3083cef862641d2a16031bec8cf81de061d1a8
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-28 10:46:07 +0530

    Time: 3 ms (64.79%) | Memory: 44.5 MB (54.42%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,28 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         int left=0;
+//         int right=s.length()-1;
+//         while(left<right){
+//         while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+//             left++;
+//         }
+//         while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+//             right--;
+//         }
+//         if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+//             return false; 
+//         }
+//         left++;
+//         right--;
+//         }return true;
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(String s) {
         int left=0;
         int right=s.length()-1;
+        s = s.toLowerCase();
         while(left<right){
         while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
             left++;
@@ -9,8 +30,8 @@ class Solution {
         while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
             right--;
         }
-        if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
-                return false; 
+        if (s.charAt(left) != s.charAt(right)){
+            return false; 
         }
         left++;
         right--;
