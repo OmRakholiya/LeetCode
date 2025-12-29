e2dc29ee0f084ea57f875d9ecd349c0dbfbe5294
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-29 10:33:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (27.69%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -38,30 +38,6 @@
 // }
 
 
-// class Solution {
-//     public int findGCD(int[] nums) {
-//         int min=Integer.MAX_VALUE;
-//         int max=Integer.MIN_VALUE;
-
-//         for(int num : nums){
-//             min = Math.min(min,num);
-//             max = Math.max(max,num);
-//         }
-//         return gcd(min,max);
-//     }
-//     public int gcd(int a,int b){
-//         while(b>0 && a>0){
-//             if(a>b) a=a%b;
-//             else b=b%a;
-//         }
-//         if(a==0){
-//             return b;
-//         }
-//         return a;
-//     }
-// }
-
-
 class Solution {
     public int findGCD(int[] nums) {
         int min=Integer.MAX_VALUE;
@@ -74,9 +50,33 @@ class Solution {
         return gcd(min,max);
     }
     public int gcd(int a,int b){
+        while(b>0 && a>0){
+            if(a>b) a=a%b;
+            else b=b%a;
+        }
         if(b==0){
             return a;
         }
-        return gcd(b,a%b);
+        return b;
     }
 }
+
+
+// class Solution {
+//     public int findGCD(int[] nums) {
+//         int min=Integer.MAX_VALUE;
+//         int max=Integer.MIN_VALUE;
+
+//         for(int num : nums){
+//             min = Math.min(min,num);
+//             max = Math.max(max,num);
+//         }
+//         return gcd(min,max);
+//     }
+//     public int gcd(int a,int b){
+//         if(b==0){
+//             return a;
+//         }
+//         return gcd(b,a%b);
+//     }
+// }
