8712dc266be4fbc8d992a849f6de04a25b6ed051
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-29 10:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (81.92%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -1,15 +1,82 @@
+// class Solution {
+//     public int findGCD(int[] nums) {
+//         Arrays.sort(nums);
+//         int min=nums[0];
+//         int gcd=1;
+//         int max=nums[nums.length-1];
+
+//         for(int i=1;i<=min;i++){
+//             if(min %i == 0 & max % i == 0){
+//                 gcd=i;
+//             }
+//         }
+//         return gcd;
+//     }
+// }
+
+
+
+// class Solution {
+//     public int findGCD(int[] nums) {
+//         int min=Integer.MAX_VALUE;
+//         int max=Integer.MIN_VALUE;
+
+//         for(int num : nums){
+//             min = Math.min(min,num);
+//             max = Math.max(max,num);
+//         }
+//         return gcd(min,max);
+//     }
+//     public int gcd(int a,int b){
+//         while(b!=0){
+//             int tem = b;
+//             b = a % b;
+//             a = tem;
+//         }
+//         return a;
+//     }
+// }
+
+
+// class Solution {
+//     public int findGCD(int[] nums) {
+//         int min=Integer.MAX_VALUE;
+//         int max=Integer.MIN_VALUE;
+
+//         for(int num : nums){
+//             min = Math.min(min,num);
+//             max = Math.max(max,num);
+//         }
+//         return gcd(min,max);
+//     }
+//     public int gcd(int a,int b){
+//         while(b>0 && a>0){
+//             if(a>b) a=a%b;
+//             else b=b%a;
+//         }
+//         if(a==0){
+//             return b;
+//         }
+//         return a;
+//     }
+// }
+
+
 class Solution {
     public int findGCD(int[] nums) {
-        Arrays.sort(nums);
-        int min=nums[0];
-        int gcd=1;
-        int max=nums[nums.length-1];
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
 
-        for(int i=1;i<=min;i++){
-            if(min %i == 0 & max % i == 0){
-                gcd=i;
-            }
+        for(int num : nums){
+            min = Math.min(min,num);
+            max = Math.max(max,num);
         }
-        return gcd;
+        return gcd(min,max);
+    }
+    public int gcd(int a,int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
     }
 }
