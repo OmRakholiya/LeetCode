a426e586ff0f8743539c4bcbcadf1168019e8e2a
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-29 10:54:30 +0530

    Time: 1 ms (94.33%) | Memory: 43.8 MB (57.42%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -19,18 +19,34 @@
 // }
 
 
+// class Solution {
+//     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+//         List<Boolean> list = new ArrayList<>();
+//         for(int i=0;i<candies.length;i++){
+//             boolean isgretest = true;
+//             for(int j=0;j<candies.length;j++){
+//                 if(candies[i]+extraCandies < candies[j]){
+//                     isgretest = false;
+//                     break;
+//                 }
+//             }
+//             list.add(isgretest);
+//         }
+//         return list;
+//     }
+// }
+
+
+
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> list = new ArrayList<>();
-        for(int i=0;i<candies.length;i++){
-            boolean isgretest = true;
-            for(int j=0;j<candies.length;j++){
-                if(candies[i]+extraCandies < candies[j]){
-                    isgretest = false;
-                    break;
-                }
-            }
-            list.add(isgretest);
+        int max=0;
+        for(int candy : candies){
+            max = Math.max(max,candy);
+        }
+        for(int candy : candies){ 
+            list.add(candy + extraCandies >= max) ;         
         }
         return list;
     }
