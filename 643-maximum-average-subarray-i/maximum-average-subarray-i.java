bcfcc97d21e26e2ad1f4905e676e154af9b42515
OM RAKHOLIYA <omrakholiya950@gmail.com>
2025-12-31 10:20:13 +0530

    Time: 2 ms (99.65%) | Memory: 69.6 MB (57.21%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,3 +1,18 @@
+// class Solution {
+//     public double findMaxAverage(int[] nums, int k) {
+//         double maxAvg = -Double.MAX_VALUE;
+//         for(int i=0;i<=nums.length-k;i++){
+//             int sum=0;
+//             for(int j=i;j<i+k;j++){
+//                 sum+=nums[j];
+//             }
+//             maxAvg = Math.max(maxAvg,(double)sum/k);
+//         }
+//         return maxAvg;
+//     }
+// }
+
+
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int sum=0;
