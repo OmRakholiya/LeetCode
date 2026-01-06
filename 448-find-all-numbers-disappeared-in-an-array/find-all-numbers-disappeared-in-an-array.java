b636e0e467d7e39940201d0009049da178f5d127
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-06 16:19:32 +0530

    Time: 7 ms (48.94%) | Memory: 67.1 MB (75.40%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,14 +1,36 @@
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         Set<Integer> seen=new HashSet<>();
+//         for(int num:nums){
+//             seen.add(num);
+//         }
+//         List<Integer> res=new ArrayList<>();
+//         for(int i=1;i<=nums.length;i++){
+//             if(!seen.contains(i)){
+//                 res.add(i);
+//             }
+//         }return res;
+//     }
+// }
+
+
+
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        Set<Integer> seen=new HashSet<>();
-        for(int num:nums){
-            seen.add(num);
-        }
-        List<Integer> res=new ArrayList<>();
-        for(int i=1;i<=nums.length;i++){
-            if(!seen.contains(i)){
-                res.add(i);
+        for(int i=0;i<nums.length;i++){
+            int idx=Math.abs(nums[i])-1;
+            if(nums[idx]>0){
+                nums[idx]= -nums[idx];
             }
-        }return res;
+        }
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<=nums.length-1;i++){
+            if(nums[i]>0){
+                ans.add(i+1);
+            }
+        }return ans;
     }
 }
+
+
+
