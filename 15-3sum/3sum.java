f673556d76c66e44ebf92b452474bbcd6efe2552
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-07 16:50:00 +0530

    Time: 36 ms (32.83%) | Memory: 59.1 MB (61.52%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -17,22 +17,59 @@
 // }
 
 
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         int n=nums.length;
+//         Set<List<Integer>> res = new HashSet<>();
+//         Arrays.sort(nums);
+//         for(int i=0;i<n;i++){
+//             Set<Integer> set =new HashSet<>();
+//             for(int j=i+1;j<n;j++){
+//                 int third = -nums[i]-nums[j];
+
+//                 if(set.contains(third)){
+//                     res.add(Arrays.asList(nums[i],nums[j],third));
+//                 }
+//                 set.add(nums[j]);
+//             }
+//         }
+//         return new ArrayList<>(res);
+//     }
+// }
+
+
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n=nums.length;
-        Set<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            Set<Integer> set =new HashSet<>();
-            for(int j=i+1;j<n;j++){
-                int third = -nums[i]-nums[j];
 
-                if(set.contains(third)){
-                    res.add(Arrays.asList(nums[i],nums[j],third));
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int l=i+1;
+            int r=n-1;
+
+            while(l<r){
+                int sum = nums[i]+nums[l]+nums[r];
+
+                if(sum==0){
+                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
+
+                    while(l<r && nums[l]==nums[l+1]) l++;
+                    while(l<r && nums[r]==nums[r-1]) r--;
+                    l++;
+                    r--;
                 }
-                set.add(nums[j]);
+                else if(sum<0){
+                    l++;
+                }
+                else{
+                    r--;
+                }
             }
+        
         }
-        return new ArrayList<>(res);
+        return res;
     }
 }
