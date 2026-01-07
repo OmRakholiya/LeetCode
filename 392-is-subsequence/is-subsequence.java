b9c77e7fcaa2e69317486fe02565bb5e5456e2ee
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-07 09:31:47 +0530

    Time: 1 ms (93.84%) | Memory: 43 MB (27.62%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,23 +1,36 @@
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         char[] s1 = s.toCharArray();
+//         char[] s2 = t.toCharArray();
+//         int i=0,j=0;
+//         while(i<s1.length && j<s2.length){
+//             if(s1[i]==s2[j]){
+//                 i++;
+//             }
+//             j++;
+//         }return i==s1.length;
+//     }
+// }
+
+        
+
+
+
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        char[] s1 = s.toCharArray();
-        char[] s2 = t.toCharArray();
-        int i=0,j=0;
-        while(i<s1.length && j<s2.length){
-            if(s1[i]==s2[j]){
-                i++;
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            boolean found = false;
+            while(j<t.length()){
+                if(s.charAt(i)==t.charAt(j)){
+                    found = true;
+                    j++;
+                    break;
+                }
+                j++;
             }
-            j++;
-        }return i==s1.length;
+            if(!found) return false;
+        }
+        return true;
     }
 }
-
-    // char[] s1 = s.toCharArray();
-    //     char[] s2 = t.toCharArray();
-    //     for(int i=0;i<s.length();i++){
-    //         for(int j=0;j<t.length();j++){
-    //             if(s1[i]!=s2[j]){
-    //                 return false;
-    //             }
-    //         }
-    //     }return false;
