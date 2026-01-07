4b11b83b96eca24fa637a3f9ed98d226e3e936b3
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-07 09:34:05 +0530

    Time: 2 ms (67.31%) | Memory: 43.2 MB (7.79%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,36 +1,34 @@
-// class Solution {
-//     public boolean isSubsequence(String s, String t) {
-//         char[] s1 = s.toCharArray();
-//         char[] s2 = t.toCharArray();
-//         int i=0,j=0;
-//         while(i<s1.length && j<s2.length){
-//             if(s1[i]==s2[j]){
-//                 i++;
-//             }
-//             j++;
-//         }return i==s1.length;
-//     }
-// }
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0,j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+            }
+            j++;
+        }return i==s.length();
+    }
+}
 
         
 
 
 
-class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int j=0;
-        for(int i=0;i<s.length();i++){
-            boolean found = false;
-            while(j<t.length()){
-                if(s.charAt(i)==t.charAt(j)){
-                    found = true;
-                    j++;
-                    break;
-                }
-                j++;
-            }
-            if(!found) return false;
-        }
-        return true;
-    }
-}
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         int j=0;
+//         for(int i=0;i<s.length();i++){
+//             boolean found = false;
+//             while(j<t.length()){
+//                 if(s.charAt(i)==t.charAt(j)){
+//                     found = true;
+//                     j++;
+//                     break;
+//                 }
+//                 j++;
+//             }
+//             if(!found) return false;
+//         }
+//         return true;
+//     }
+// }
