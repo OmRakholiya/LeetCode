1116a82d4e803f2b3c67a3f937b5118910f72193
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-09 19:02:05 +0530

    Time: 4 ms (87.89%) | Memory: 46.4 MB (89.03%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -18,20 +18,43 @@
 
 
 
+    // class Solution {
+    //     public String reverseWords(String s) {
+    //         StringBuilder res = new StringBuilder();
+    //         String[] words = s.split(" ");
 
-    class Solution {
-        public String reverseWords(String s) {
-            StringBuilder res = new StringBuilder();
-            String[] words = s.split(" ");
-
-            for(int i=0;i<words.length;i++){
-                StringBuilder sb = new StringBuilder(words[i]);
-                res.append(sb.reverse());
+    //         for(int i=0;i<words.length;i++){
+    //             StringBuilder sb = new StringBuilder(words[i]);
+    //             res.append(sb.reverse());
                 
-                if(i!=words.length-1){
-                    res.append(" ");
-                }
+    //             if(i!=words.length-1){
+    //                 res.append(" ");
+    //             }
+    //         }
+    //         return res.toString();
+    //     }
+    // }
+
+
+class Solution {
+    public String reverseWords(String s) {
+        char[] arr = s.toCharArray();
+        int start=0;
+        for(int end=0;end<=arr.length;end++){
+            if(end == arr.length || arr[end] == ' '){
+                reverse(arr,start,end-1);
+                start=end+1;
             }
-            return res.toString();
+        }
+        return  new String(arr);
+    }
+    private void reverse(char[] arr,int l,int r){
+        while(l<r){
+            char tem = arr[l];
+            arr[l] = arr[r];
+            arr[r] = tem;
+            l++;
+            r--;
         }
     }
+}
