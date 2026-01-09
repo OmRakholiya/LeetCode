62df0a12a42c2868d2e3a947adf46bc7a24de5e1
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-09 18:55:57 +0530

    Time: 4 ms (87.89%) | Memory: 47.2 MB (20.98%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,17 +1,37 @@
-class Solution {
-    public String reverseWords(String s) {
-        String res = "";
-        String[] words = s.split(" ");
-        for(int i=0;i<words.length;i++){
-            String rev = "";
-            for(int j=words[i].length()-1;j>=0;j--){
-                rev += words[i].charAt(j);
+// class Solution {
+//     public String reverseWords(String s) {
+//         String res = "";
+//         String[] words = s.split(" ");
+//         for(int i=0;i<words.length;i++){
+//             String rev = "";
+//             for(int j=words[i].length()-1;j>=0;j--){
+//                 rev += words[i].charAt(j);
+//             }
+//             res += rev;
+//             if(i!=words.length-1){
+//                 res += " ";
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+
+
+
+    class Solution {
+        public String reverseWords(String s) {
+            StringBuilder res = new StringBuilder();
+            String[] words = s.split(" ");
+
+            for(int i=0;i<words.length;i++){
+                StringBuilder sb = new StringBuilder(words[i]);
+                res.append(sb.reverse());
+                
+                if(i!=words.length-1){
+                    res.append(" ");
+                }
             }
-            res += rev;
-            if(i!=words.length-1){
-                res += " ";
-            }
+            return res.toString();
         }
-        return res;
     }
-}
