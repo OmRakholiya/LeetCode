84182234bbc79105aa3e264d4c26353eb1a81b14
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-10 14:24:36 +0530

    Time: 1 ms (99.68%) | Memory: 77.4 MB (22.39%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,28 @@
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int curr=nums[0];
+//         int max=nums[0];
+//         for(int i=1;i<nums.length;i++){
+//             curr=Math.max(nums[i],curr+nums[i]);
+//             max=Math.max(max,curr);
+//         }
+//         return max;
+//     }
+// }
+
+
 class Solution {
     public int maxSubArray(int[] nums) {
-        int curr=nums[0];
-        int max=nums[0];
-        for(int i=1;i<nums.length;i++){
-            curr=Math.max(nums[i],curr+nums[i]);
-            max=Math.max(max,curr);
+        int currSum=0;
+        int maxSum=nums[0];
+        for(int num : nums){
+            currSum += num;
+            maxSum=Math.max(maxSum,currSum);
+            if(currSum<0){
+                currSum=0;
+            }
         }
-        return max;
+        return maxSum;
     }
 }
 
@@ -14,13 +30,12 @@ class Solution {
 //     public int maxSubArray(int[] nums) {
 //         int maxsum=Integer.MIN_VALUE;
 //         for(int i=0;i<nums.length;i++){
-//             for(int j=i;j<nums.length;j++){
-//                 int sum=0;
-//                 for(int k=i;k<=j;k++){                    
-//                     sum+=nums[k];
-//                 }
+//             int sum=0; 
+//             for(int j=i;j<nums.length;j++){      
+//                 sum+=nums[j];
 //                 maxsum=Math.max(maxsum,sum);
 //             }
 //         }return maxsum;
 //     }
 // }
+
