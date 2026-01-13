8bbb66e1dc5f68405db312dcede593c4ed995b0e
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-13 16:49:44 +0530

    Time: 1 ms (17.55%) | Memory: 48.3 MB (35.10%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,58 @@
+// class Solution {
+//     public int[] searchRange(int[] nums, int target) {
+//        int res[]={-1,-1};
+//        res[0]=first(nums,target);
+//        res[1]=second(nums,target);
+//        return res;
+//     }
+//     public int first(int[] nums,int target){
+//         int index=-1;
+//         int l=0,r=nums.length-1;
+//         while(l<=r){
+//             int mid=(l+r)/2;
+//             if(target==nums[mid]){
+//                 index=mid;
+//                 r=mid-1;
+//                 }
+//             else if(target>nums[mid])
+//                 l=mid+1;
+//             else
+//                 r=mid-1;
+//         }return index;
+
+//     }
+//     public int second(int[] nums,int target){
+//         int index=-1;
+//         int l=0,r=nums.length-1;
+//         while(l<=r){
+//             int mid=(l+r)/2;
+//             if(target==nums[mid]){
+//                 index=mid;
+//                 l=mid+1;
+//                 }
+//             else if(target>nums[mid])
+//                 l=mid+1;
+//             else
+//                 r=mid-1;
+//         }return index;
+//     }
+
+// }
+
+
+
+
+
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-       int res[]={-1,-1};
-       res[0]=first(nums,target);
-       res[1]=second(nums,target);
-       return res;
-    }
-    public int first(int[] nums,int target){
-        int index=-1;
-        int l=0,r=nums.length-1;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(target==nums[mid]){
-                index=mid;
-                r=mid-1;
-                }
-            else if(target>nums[mid])
-                l=mid+1;
-            else
-                r=mid-1;
-        }return index;
+        int first = -1, last = -1;
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (first == -1) first = i;
+                last = i;
+            }
+        }
+        return new int[]{first, last};
     }
-    public int second(int[] nums,int target){
-        int index=-1;
-        int l=0,r=nums.length-1;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(target==nums[mid]){
-                index=mid;
-                l=mid+1;
-                }
-            else if(target>nums[mid])
-                l=mid+1;
-            else
-                r=mid-1;
-        }return index;
-    }
-
 }
