2345e16f7671168a81edd64ec2656699d4bc30ba
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-15 16:41:14 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (76.63%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,44 @@
-// class Solution {
-//     public int[] searchRange(int[] nums, int target) {
-//        int res[]={-1,-1};
-//        res[0]=first(nums,target);
-//        res[1]=second(nums,target);
-//        return res;
-//     }
-//     public int first(int[] nums,int target){
-//         int index=-1;
-//         int l=0,r=nums.length-1;
-//         while(l<=r){
-//             int mid=(l+r)/2;
-//             if(target==nums[mid]){
-//                 index=mid;
-//                 r=mid-1;
-//                 }
-//             else if(target>nums[mid])
-//                 l=mid+1;
-//             else
-//                 r=mid-1;
-//         }return index;
+class Solution {
+    
+    public int[] searchRange(int[] nums, int target) {
+       int res[]={-1,-1};
+       res[0]=first(nums,target);
+       res[1]=second(nums,target);
+       return res;
+    }   
 
-//     }
-//     public int second(int[] nums,int target){
-//         int index=-1;
-//         int l=0,r=nums.length-1;
-//         while(l<=r){
-//             int mid=(l+r)/2;
-//             if(target==nums[mid]){
-//                 index=mid;
-//                 l=mid+1;
-//                 }
-//             else if(target>nums[mid])
-//                 l=mid+1;
-//             else
-//                 r=mid-1;
-//         }return index;
-//     }
+    public int first(int[] nums,int target){
+        int index=-1;
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==target){
+                index=mid;
+                r=mid-1;
+            }
+            else if(nums[mid]<target)
+                l=mid+1;
+            else
+                r=mid-1;
+        }return index;
 
-// }
+    }
+    public int second(int[] nums,int target){
+        int index=-1;
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(target==nums[mid]){
+                index=mid;
+                l=mid+1;
+                }
+            else if(target>nums[mid])
+                l=mid+1;
+            else
+                r=mid-1;
+        }return index;
+    }
+}
 
 
 
@@ -60,34 +61,34 @@
 
 
 
-class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int n=nums.length;
-        int l=0;
-        int h=n-1;
-        int idx = -1;
+// class Solution {
+//     public int[] searchRange(int[] nums, int target) {
+//         int n=nums.length;
+//         int l=0;
+//         int h=n-1;
+//         int idx = -1;
 
-        while(l<=h){
-            int mid = l+(h-l)/2;
-            if(nums[mid]==target){
-                idx=mid;
-                break;
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else{
-                h=mid-1;
-            }
-        }
-        if(idx == -1) return new int[]{-1,-1};
+//         while(l<=h){
+//             int mid = l+(h-l)/2;
+//             if(nums[mid]==target){
+//                 idx=mid;
+//                 break;
+//             }
+//             else if(nums[mid]<target){
+//                 l=mid+1;
+//             }
+//             else{
+//                 h=mid-1;
+//             }
+//         }
+//         if(idx == -1) return new int[]{-1,-1};
 
-        int left=idx;
-        int right=idx;
+//         int left=idx;
+//         int right=idx;
 
-        while(left-1 >= 0 && nums[left-1] == target) left--;
-        while(right+1 < n && nums[right+1] == target) right++;
+//         while(left-1 >= 0 && nums[left-1] == target) left--;
+//         while(right+1 < n && nums[right+1] == target) right++;
 
-        return new int[]{left,right};
-    }
-}
+//         return new int[]{left,right};
+//     }
+// }
