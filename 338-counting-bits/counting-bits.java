abacd5a448bf0693a02389292a0b703a95cf8e7d
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-17 21:57:51 +0530

    Time: 2 ms (96.42%) | Memory: 48.9 MB (9.31%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,18 +1,32 @@
+// class Solution {
+//     public int[] countBits(int n) {
+//         int[] ans = new int[n+1];
+
+//         for(int i=0;i<=n;i++){
+//             String binary = Integer.toBinaryString(i);
+//             int count=0;
+
+//             for(int j=0;j<binary.length();j++){
+//                 if(binary.charAt(j)=='1'){
+//                     count++;
+//                 }
+//             }
+//             ans[i]=count;
+//         }
+//         return ans; 
+//     }
+// }
+
+
+
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n+1];
-
-        for(int i=0;i<=n;i++){
-            String binary = Integer.toBinaryString(i);
-            int count=0;
-
-            for(int j=0;j<binary.length();j++){
-                if(binary.charAt(j)=='1'){
-                    count++;
-                }
-            }
-            ans[i]=count;
-        }
-        return ans; 
+       int[] ans = new int[n+1];
+       for(int i=0;i<=n;i++){
+          ans[i] = ans[i >> 1] + (i & 1);
+       }
+       return ans;
     }
 }
+
+
