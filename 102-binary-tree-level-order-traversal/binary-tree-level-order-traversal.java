9dbeb5ea360a75b834f44c5cda042d8db8fb6853
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-01-28 09:49:43 +0530

    Time: 1 ms (95.28%) | Memory: 46.7 MB (86.17%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -13,22 +13,53 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> ans = new ArrayList<>();
+        
+//         dfs(root,0,ans);
+//         return ans;
+
+//     }
+//     private void dfs(TreeNode root,int level,List<List<Integer>> ans){
+//         if(root==null) return;
+
+//         if(level==ans.size()){
+//             ans.add(new ArrayList<>());
+//         } 
+//         ans.get(level).add(root.val);
+
+//         dfs(root.left,level+1,ans);
+//         dfs(root.right,level+1,ans);
+
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        
-        dfs(root,0,ans);
+        if(root==null) return ans;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i=0;i<size;i++){
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if(node.left!=null) 
+                    queue.offer(node.left);
+
+                if(node.right!=null) 
+                    queue.offer(node.right);
+
+            }
+            ans.add(level);
+        }
         return ans;
-
-    }
-    private void dfs(TreeNode root,int level,List<List<Integer>> ans){
-        if(root==null) return;
-
-        if(level==ans.size()) ans.add(new ArrayList<>());
-        ans.get(level).add(root.val);
-
-        dfs(root.left,level+1,ans);
-        dfs(root.right,level+1,ans);
-
     }
 }
