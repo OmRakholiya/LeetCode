a60d6c992887c3732332024f22c97d9a9f8e3fef
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-02-03 18:01:38 +0530

    Time: 5 ms (3.36%) | Memory: 44.8 MB (49.58%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -1,20 +1,52 @@
+// class Solution {
+//     public boolean isTrionic(int[] nums) {
+//     int n =  nums.length;
+//     int i=0;
+//     while(i+1<n && nums[i]<nums[i+1]){
+//         i++;        
+//     }
+//     if(i==0) return false;
+
+//     while(i+1<n && nums[i]>nums[i+1]){
+//         i++;
+//     }
+//     if(i==n-1) return false;
+
+//     while(i+1<n && nums[i]<nums[i+1]){
+//         i++;
+//     }
+//     return i==n-1;
+//     }
+// }
+
+
 class Solution {
     public boolean isTrionic(int[] nums) {
-    int n =  nums.length;
-    int i=0;
-        while(i+1<n && nums[i]<nums[i+1]){
-            i++;        
+        int n= nums.length;
+        for(int p=1;p<=n-3;p++){
+            for(int q=p+1;q<=n-2;q++){
+                boolean found = true;
+                for(int i=0;i<p;i++){
+                    if(nums[i]>=nums[i+1]){
+                        found=false;
+                        break;
+                    }
+                }
+                for(int i=p;i<q && found;i++){
+                    if(nums[i]<=nums[i+1]){
+                        found=false;
+                        break;
+                    }
+                }
+                for(int i=q;i<n-1 && found;i++){
+                    if(nums[i]>=nums[i+1]){
+                        found=false;
+                        break;
+                    }
+                }
+                if(found) return true;
+            }
         }
-        if(i==0) return false;
-
-        while(i+1<n && nums[i]>nums[i+1]){
-            i++;
-        }
-        if(i==n-1) return false;
-
-        while(i+1<n && nums[i]<nums[i+1]){
-            i++;
-        }
-        return i==n-1;
+        return false;
     }
 }
