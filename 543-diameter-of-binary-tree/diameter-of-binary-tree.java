f8fa991624462650a85ceccf222231a4f3eb056f
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-02-05 21:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (63.98%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+
 class Solution {
     int diameter = 0;
     public int diameterOfBinaryTree(TreeNode root) {
@@ -24,6 +25,21 @@ class Solution {
         int lh = height(node.left);
         int rh = height(node.right);
         diameter = Math.max(diameter, lh+rh);
-        return 1+Math.max(lh,rh);
+        return 1 + Math.max(lh,rh);
     }
 }
+
+// class Solution {
+//     int max = 0;
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         height(root);
+//         return max;
+//     }
+//     int height(TreeNode node){
+//         if(node==null) return 0;
+//         int lh = height(node.left);
+//         int rh = height(node.right);
+//         max = Math.max(max, lh+rh);
+//         return 1 + Math.max(lh,rh);
+//     }
+// }
