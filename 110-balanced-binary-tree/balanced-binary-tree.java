e54fbc2f054448e12286811e6b9b1a0cc0939ae2
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-02-10 21:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (61.32%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -13,19 +13,40 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+//         if(root == null) return true;
+
+//         int lh = height(root.left);
+//         int rh = height(root.right);
+
+//         if(Math.abs(lh-rh)>1) return false;
+
+//         return isBalanced(root.left) && isBalanced(root.right); 
+//     }
+//     int height(TreeNode node){
+//         if(node==null) return 0;
+//         return 1 + Math.max(height(node.left),height(node.right));
+//     }
+// }
+
+
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-
-        int lh = height(root.left);
-        int rh = height(root.right);
-
-        if(Math.abs(lh-rh)>1) return false;
-
-        return isBalanced(root.left) && isBalanced(root.right); 
+        return height(root) != -1; 
     }
     int height(TreeNode node){
         if(node==null) return 0;
-        return 1 + Math.max(height(node.left),height(node.right));
+        
+        int lh = height(node.left);
+        if(lh == -1) return -1;
+
+        int rh = height(node.right);
+        if(rh ==-1) return -1;
+
+        if(Math.abs(lh-rh) > 1) return -1;
+
+        return 1 + Math.max(lh,rh);
+
     }
 }
