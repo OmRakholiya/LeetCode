19336c4077990cb0f57e648cea514a28fa93c703
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-02-10 13:26:56 +0530

    Time: 497 ms (5.11%) | Memory: 47.1 MB (45.62%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -1,16 +1,41 @@
+// class Solution {
+//     public int longestBalanced(int[] nums) {
+//         int maxLen=0;
+//         for(int i=0;i<nums.length;i++){
+//             Set<Integer> even = new HashSet<>();
+//             Set<Integer> odd = new HashSet<>();
+
+//             for(int j=i;j<nums.length;j++){
+//                 if(nums[j]%2==0){
+//                     even.add(nums[j]);
+//                 }
+//                 else{
+//                     odd.add(nums[j]);
+//                 }
+//                 if(even.size() == odd.size()){
+//                     maxLen = Math.max(maxLen , j-i+1);
+//                 }
+//             }
+//         }
+//         return maxLen;
+//     }
+// }
+
+
+
 class Solution {
     public int longestBalanced(int[] nums) {
         int maxLen=0;
         for(int i=0;i<nums.length;i++){
-            Set<Integer> even = new HashSet<>();
-            Set<Integer> odd = new HashSet<>();
+            Map<Integer,Integer> even = new HashMap<>();
+            Map<Integer,Integer> odd = new HashMap<>();
 
             for(int j=i;j<nums.length;j++){
                 if(nums[j]%2==0){
-                    even.add(nums[j]);
+                    even.put(nums[j],even.getOrDefault(nums[j],0)+1);
                 }
                 else{
-                    odd.add(nums[j]);
+                    odd.put(nums[j],odd.getOrDefault(nums[j],0)+1);
                 }
                 if(even.size() == odd.size()){
                     maxLen = Math.max(maxLen , j-i+1);
