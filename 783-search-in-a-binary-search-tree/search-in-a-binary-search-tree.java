8195c021a14277290dbc84b139e97c001f84dae7
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-02-13 11:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (51.07%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -13,35 +13,35 @@
  *     }
  * }
  */
-// class Solution {
-//     public TreeNode searchBST(TreeNode root, int val) {
-//         if(root == null){
-//             return null;
-//         }  
-
-//         if(root.val == val) return root;
-        
-//         if(root.val < val){
-//             return searchBST(root.right,val);
-//         }
-//         return searchBST(root.left,val);
-//     }
-// }
-
-
-
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        while(root != null){
-            if(root.val == val) return root;
-        
-            if(root.val < val){
-                root = root.right;
-            }
-            else{
-                root = root.left;
-            }
+        if(root == null){
+            return null;
         }  
-        return null;
+
+        if(root.val == val) return root;
+        
+        if(root.val < val){
+            return searchBST(root.right,val);
+        }
+        return searchBST(root.left,val);
     }
 }
+
+
+
+// class Solution {
+//     public TreeNode searchBST(TreeNode root, int val) {
+//         while(root != null){
+//             if(root.val == val) return root;
+        
+//             if(root.val < val){
+//                 root = root.right;
+//             }
+//             else{
+//                 root = root.left;
+//             }
+//         }  
+//         return null;
+//     }
+// }
