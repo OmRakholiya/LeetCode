c38a0e8d49f981059b3a71e033e229839d2ddda2
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-02-21 10:55:28 +0530

    Time: 4 ms (87.21%) | Memory: 42.2 MB (90.65%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,28 +1,75 @@
+// class Solution {
+//     public int countPrimeSetBits(int left, int right) {
+//         int res=0;
+
+//         for(int i = left; i <= right; i++){
+//             String binary = Integer.toBinaryString(i);
+
+//             int count = 0;
+//             for(int j=0; j<binary.length(); j++){
+//                 if(binary.charAt(j)=='1'){
+//                     count++;
+//                 }
+//             }
+//             if(isPrime(count)){
+//                 res++;
+//             }
+//         }
+//         return res;
+//     }
+//     public boolean isPrime(int n){
+//         if (n <= 1) return false;
+
+//         for (int i = 2; i < n; i++) {
+//             if (n % i == 0) return false;
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
+// class Solution {
+//     public int countPrimeSetBits(int left, int right) {
+//         int res=0;
+
+//         for(int i = left; i <= right; i++){
+//             String binary = Integer.toBinaryString(i);
+
+//             int count = Integer.bitCount(i);
+//             if(isPrime(count)){
+//                 res++;
+//             }
+//         }
+//         return res;
+//     }
+//     public boolean isPrime(int n){
+//         if (n <= 1) return false;
+
+//         for (int i = 2; i < n; i++) {
+//             if (n % i == 0) return false;
+//         }
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public int countPrimeSetBits(int left, int right) {
         int res=0;
 
         for(int i = left; i <= right; i++){
-            String binary = Integer.toBinaryString(i);
+            int count = Integer.bitCount(i);
 
-            int count = 0;
-            for(int j=0; j<binary.length(); j++){
-                if(binary.charAt(j)=='1'){
-                    count++;
-                }
-            }
-            if(isPrime(count)){
+            if(count == 2 || count == 3 || count == 5 || 
+               count == 7 || count == 11 || count == 13 || 
+               count == 17 || count == 19)
+               {
                 res++;
-            }
+               }
+
         }
         return res;
-    }
-    public boolean isPrime(int n){
-        if (n <= 1) return false;
-
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
     }
 }
