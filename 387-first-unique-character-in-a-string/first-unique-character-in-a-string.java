5723e8a1b082b1ce13e50da579ee544996e2d17f
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-02-23 17:40:15 +0530

    Time: 6 ms (84.35%) | Memory: 47.4 MB (5.94%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,10 +1,29 @@
+// class Solution {
+//     public int firstUniqChar(String s) {
+//         for(int i=0;i<s.length();i++){
+//             char c = s.charAt(i);
+//             if(s.indexOf(c)==s.lastIndexOf(c)){
+//                 return i;
+//             }               
+//         }return -1;
+//     }
+// }
+
+
+
 class Solution {
     public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        
         for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(s.indexOf(c)==s.lastIndexOf(c)){
+            freq[s.charAt(i) - 'a']++;        
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']==1){
                 return i;
-            }               
-        }return -1;
+            }
+        }
+        return -1;
     }
 }
