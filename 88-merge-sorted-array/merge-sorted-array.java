de9e9cc26ef58150166e72f3aa213f329b6ab684
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-03-17 10:11:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (12.08%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,9 +1,58 @@
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int[] combined = new int[nums1.length +nums2.length];
+//         for(int i=0;i<n;i++){
+//             nums1[m+i]=nums2[i];
+//         }
+//         Arrays.sort(nums1);
+//     }    
+// }
+
+
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int l = m-1;
+//         int r = 0;
+//         while(l>=0 && r<n){
+//             if(nums1[l] > nums2[r]){
+//                 int tem = nums1[l];
+//                 nums1[l] = nums2[r];
+//                 nums2[r] = tem;
+//                 l--;
+//                 r++;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         Arrays.sort(nums1,0,m);
+//         Arrays.sort(nums2,0,n);
+
+//         for(int i=0;i<n;i++){
+//             nums1[i+m] = nums2[i];
+//         }
+//     }    
+// }
+
+
+
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] combined = new int[nums1.length +nums2.length];
-        for(int i=0;i<n;i++){
-            nums1[m+i]=nums2[i];
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while(i>=0 && j >=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }
+            else{
+                nums1[k--] = nums2[j--];
+            }
         }
-        Arrays.sort(nums1);
+        while(j >= 0){
+            nums1[k--] = nums2[j--];
+        }
     }    
 }
