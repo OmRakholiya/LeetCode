2484ad1a13611b64bf91f426107d374fac4cdf07
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-03-19 23:42:54 +0530

    Time: 2 ms (70.69%) | Memory: 55.6 MB (59.10%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,49 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         for(int i=0;i<nums.length;i++){
+//             int count=0;
+//             for(int j=0;j<nums.length;j++){
+//                 if(nums[i]==nums[j]){
+//                     count++;
+//                 }
+//             }   
+//             if(count>nums.length/2){
+//                 return nums[i];
+//             }
+//         }return -1;
+//     }
+// }
+
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         int n=nums.length;
+//         for(int num : nums){
+//             map.put(num, map.getOrDefault(num,0)+1);
+//             if(map.get(num)>n/2){
+//                 return num;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int majorityElement(int[] nums) {
-        
-        for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }   
-            if(count>nums.length/2){
-                   return nums[i];
+        int count=0;
+        int el=0;
+        for(int num : nums){
+            if(count==0){
+                el=num;
             }
-        }return -1;
+            if(num==el){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return el;
     }
 }
