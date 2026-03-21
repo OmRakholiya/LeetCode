bcb892036133b8a89d8bf971c158ba008dfc8fd6
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-03-21 15:41:04 +0530

    Time: 18 ms (14.53%) | Memory: 53.1 MB (16.56%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,17 +1,36 @@
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         List<Integer> l1=new ArrayList<>();
+//         int n = nums.length;
+//         for(int i=0;i<n;i++){
+//             int count=0;
+//             for(int j=0;j<n;j++){
+//                 if(nums[i]==nums[j]){
+//                     count++;
+//                 }
+//             }
+//             if(count>n/3 && !l1.contains(nums[i])){
+//                 l1.add(nums[i]);
+//             }
+//         }return l1;
+//     }
+// }
+
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> l1=new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        List<Integer> res=new ArrayList<>();
         int n = nums.length;
+        int mini = n/3+1;
         for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            if(map.get(nums[i])==mini){
+                res.add(nums[i]);
             }
-            if(count>n/3 && !l1.contains(nums[i])){
-                l1.add(nums[i]);
+            if(res.size()==2){
+                break;
             }
-        }return l1;
+        }return res;
     }
 }
