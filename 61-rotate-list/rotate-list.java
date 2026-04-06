110567b77acf647d8f4df6fc094146b5e7f3fd23
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-04-06 10:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (85.36%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -1,3 +1,49 @@
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+// */
+
+// class Solution {
+//     public ListNode rotateRight(ListNode head, int k) {
+
+//         if(head == null || head.next == null || k == 0) return head;
+
+//         ListNode tail = head;
+//         int len = 1;
+//         while(tail.next != null){
+//             len++;
+//             tail = tail.next;
+//         }
+//         if(k % len == 0) return head;
+//         k = k % len;
+//         tail.next = head;
+
+//         ListNode newtail = findnewtail(head,len-k);
+//         head = newtail.next;
+//         newtail.next = null;
+
+//         return head; 
+//     }
+
+//     public ListNode findnewtail(ListNode tem, int k){
+//         int count = 1;
+        
+//         while(tem != null){
+//             if(count == k) return tem;
+//             count++;
+//             tem = tem.next;
+//         }
+//         return tem;
+//     }
+// }
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -13,6 +59,7 @@ class Solution {
     public ListNode rotateRight(ListNode head, int k) {
 
         if(head == null || head.next == null || k == 0) return head;
+
         ListNode tail = head;
         int len = 1;
         while(tail.next != null){
@@ -23,19 +70,13 @@ class Solution {
         k = k % len;
         tail.next = head;
 
-        ListNode newtail = findnewtail(head,len-k);
-        head = newtail.next;
+        ListNode newtail = head;
+        for(int i=1;i<len-k;i++){
+            newtail = newtail.next;
+        }
+        ListNode newhead = newtail.next;
         newtail.next = null;
 
-        return head; 
-    }
-    public ListNode findnewtail(ListNode tem, int k){
-        int count = 1;
-        while(tem != null){
-            if(count == k) return tem;
-            count++;
-            tem = tem.next;
-        }
-        return tem;
+        return newhead; 
     }
 }
