65fa4176dabb0ffa210bc70d7a57b40216f5cf77
OM RAKHOLIYA <omrakholiya950@gmail.com>
2026-06-11 13:49:02 +0530

    Time: 3 ms (48.69%) | Memory: 45.1 MB (41.74%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,18 +1,43 @@
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         Set<Integer> resultSet = new HashSet<>();  
+//         for(int i=0;i<nums1.length;i++){
+//             for(int j=0;j<nums2.length;j++){
+//                 if(nums1[i]==nums2[j]){
+//                     resultSet.add(nums1[i]);
+//                     break;
+//                 }
+//             }
+//         }
+//         int[] result=new int[resultSet.size()];
+//         int i=0;
+//         for(int num : resultSet){
+//             result[i++]=num;
+//         }return result;
+//     }
+// }
+
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> resultSet = new HashSet<>();  
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    resultSet.add(nums1[i]);
-                    break;
-                }
+        Set<Integer> result = new HashSet<>();  
+        Set<Integer> set1 = new HashSet<>();
+
+        for(int num : nums1){
+            set1.add(num);
+        }
+        for(int num : nums2){
+            if(set1.contains(num)){
+                result.add(num);
             }
         }
-        int[] result=new int[resultSet.size()];
-        int i=0;
-        for(int num : resultSet){
-            result[i++]=num;
-        }return result;
+
+        int[] ans = new int[result.size()];
+        int index = 0;
+        for(int num : result){
+            ans[index] = num;
+            index++;
+        }
+        return ans;
     }
 }
